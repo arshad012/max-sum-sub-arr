536e5975ac859e447196a63b4aa0cc2f209114a3
arshad012 <forarshad99@gmail.com>
2024-08-09 01:16:55 +0530

    Find max sum of k size sub array && that sub array in o(n) time

--- maxSum.js
+++ maxSum.js
@@ -4,27 +4,6 @@ let arr = [8,3,-8,2,1,3,1];
 let n = arr.length;
 let k = 3;
 
-function fixed_size_sliding_window(arr, n, k) {
-    let maxSum = 0;
-    let subArr = [];
-
-    for(let i=0; i<=n-k; i++) {
-        subArr = [];
-        let sum = 0;
-
-        for(let j=i; j<i+k; j++) {
-            subArr.push(arr[j]);
-            sum += arr[j];
-        }
-        console.log(subArr);
-        sum > maxSum ? maxSum = sum : null;
-    }
-    console.log(subArr,maxSum);
-}
-
-// fixed_size_sliding_window(arr, n, k);
-
-
 function variable_size_sliding_window(arr, n, k) {
     // getting sum of first sub array of size k
     let sum = 0;    
