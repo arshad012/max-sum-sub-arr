4ea30e44ca71726296bc21f0e2c75d5019bb0abd
arshad012 <forarshad99@gmail.com>
2024-08-09 01:08:51 +0530

    Find max sub array sum & that sub array in o(n) time

---
+++ maxSum.js
@@ -0,0 +1,66 @@
+// max sub array of size k, Sliding window technique of fixed size k
+
+let arr = [8,3,-8,2,1,3,1];
+let n = arr.length;
+let k = 3;
+
+function fixed_size_sliding_window(arr, n, k) {
+    let maxSum = 0;
+    let subArr = [];
+
+    for(let i=0; i<=n-k; i++) {
+        subArr = [];
+        let sum = 0;
+
+        for(let j=i; j<i+k; j++) {
+            subArr.push(arr[j]);
+            sum += arr[j];
+        }
+        console.log(subArr);
+        sum > maxSum ? maxSum = sum : null;
+    }
+    console.log(subArr,maxSum);
+}
+
+// fixed_size_sliding_window(arr, n, k);
+
+
+function variable_size_sliding_window(arr, n, k) {
+    // getting sum of first sub array of size k
+    let sum = 0;    
+    for(let i=0; i<k; i++) {
+        sum += arr[i];
+    }
+    
+    let maxSum = sum;
+    
+    let subArrLastIndex = k-1; // It will track the last index of sub array which has max sum
+
+    // finding max sub array sum
+    let j=k;
+    while(j<n) {
+        sum += arr[j];
+        sum -= arr[j-k];
+        if(sum > maxSum) {
+            maxSum = sum;
+            subArrLastIndex = j;
+        }
+        j++;
+    }
+
+    console.log(maxSum);
+    
+    // creating sub array by the help of "subArrLastIndex" variable
+    let subArr = new Array(k);
+    let count = subArr.length-1;
+
+    // this loop will start storing the el from last so that there will no need to reverse further
+    for(let i=subArrLastIndex; i>subArrLastIndex-k; i--) {
+        subArr[count] = arr[i];
+        count--;
+    }
+
+    console.log(subArr);
+}
+
+variable_size_sliding_window(arr, n, k);
